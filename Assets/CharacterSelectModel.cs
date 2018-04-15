ed76490d7374a97f6ececa8153488ab304cf96a8
Davey Jay Belliss <daveybelliss@gmail.com>
2018-04-14 19:08:52 -0700

    Adding main menu and character select with music

---
+++ Assets/CharacterSelectModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectModel : MonoBehaviour {
+
+    [SerializeField]
+    int playerNum = 1; // Which player this model represents
+
+    public void SetPlayerModel(int characterNum) {
+        if (characterNum == 1) {
+            // Display Main player
+        }
+        else if (characterNum == -1) {
+            // Display Random symbol
+        }
+    }
+}
---
+++ Assets/Davey/Scripts/MainMenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+    public void ChangeToMainMenu() {
+        GameManager.instance.ChangeToMainMenu ();
+    }
+
+    public void ChangeToCharacterSelect() {
+        GameManager.instance.ChangeToCharacterSelect ();
+    }
+
+    public void ChangeToFight() {
+        GameManager.instance.ChangeToFight ();
+    }
+}
