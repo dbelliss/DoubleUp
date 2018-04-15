fd842ad86fa7cef9f8503e53b94051253a4f4214
nanadvi <nanadvi@ucdavis.edu>
2018-04-14 17:55:01 -0700

    Player controller

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    private GameObject _curPlayer;
+    [SerializeField]
+    private Vector3 baseOffset;
+
+    [SerializeField]
+    float rightCameraMax;
+
+    [SerializeField]
+    float leftCameraMax;
+
+    public bool isFollowing = true; // True if camera should follow player
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_curPlayer == null)
+        {
+            _curPlayer = GameObject.Find("SeanLegacy");
+        }
+        if (_curPlayer && isFollowing)
+        {
+            this.gameObject.transform.position = _curPlayer.transform.position + baseOffset; // Move to a position tha maintains the same offset
+
+            // Check if camera is within bounds
+            if (transform.position.x > rightCameraMax) {
+                // Set to right max position
+                transform.position = new Vector3(rightCameraMax, transform.position.y, transform.position.z);
+            }
+            else if (transform.position.x < leftCameraMax)
+            {
+                // Set to max left position
+                transform.position = new Vector3(leftCameraMax, transform.position.y, transform.position.z);
+            }
+        }
+        if (_curPlayer && this.GetComponent<AudioListener>().enabled == true)
+        {
+            // Move audio listener onto the player
+            //            _curPlayer.AddComponent<AudioListener> ();
+            //            this.GetComponent<AudioListener> ().enabled = false;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    private float PlayerSpeed { get; set; }
+    private float JumpSpeed { get; set; }
+
+    // PlayerID 0 and 1
+    private int PlayerID
+    {
+        get;
+        set;
+    }
+    // Player health
+    private int PlayerHealth
+    {
+        get;
+        set;
+    }
+
+    private bool _jumpPressed = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayerSpeed = 5f;
+        JumpSpeed = 8f;
+    }
+
+    // return 0 if left 1 right 
+
+    // return 0 if left 1 right 
+    void CheckMovement()
+    {
+        int xDirection = ((int)Input.GetAxisRaw("Horizontal") < 0)
+            ? -1 : ((int)Input.GetAxisRaw("Horizontal") > 0) ? 1 : 0;
+        Vector3 move = new Vector3(xDirection, 0, 0);
+        this.transform.position += move * PlayerSpeed * Time.deltaTime;
+    }
+
+    void CheckJump()
+    {
+        int yDirection = Input.GetKeyDown("space") ? 1 : 0;
+        Vector3 jump = new Vector3(0, yDirection, 0);
+        this.GetComponent<Rigidbody>().AddForce(jump * JumpSpeed, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMovement();
+        CheckJump();
+    }
+}
